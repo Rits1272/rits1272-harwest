6cbe4d9c45368bc68507e4184b7b3846aca31fa8
Ritik Jain <ritikjain1272@gmail.com>
2020-10-20 15:16:00 +0530

    Add solution for problem `A - Little Pony and Expected Maximum` Link: https://codeforces.com/contest/453/problem/A Tags: probabilities, *1600 Ref: https://codeforces.com/contest/453/submission/96053694

--- codeforces/453/A.cpp
+++ codeforces/453/A.cpp
@@ -16,10 +16,10 @@ int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
     int n, m; cin >> m >> n;
-    double ans = m;
-    for(int i = 1; i < m; i++){
-	ans -= pow((double)i/m, n);
-    }
+    double ans = 0;
+    for(int i = 1; i <= m; i++){
+	ans += (pow((double)i/m, n) - pow((double)(i-1)/m, n)) * i;
+    }	
     cout << fixed << setprecision(9) << ans << '\n';
     return 0;
 }
