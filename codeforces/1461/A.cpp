6a4eb7387ef612953a7f114e5ca0d23113120e29
Ritik Jain <ritikjain1272@gmail.com>
2020-12-11 20:16:00 +0530

    Add solution for problem `A - String Generation` Link: https://codeforces.com/contest/1461/problem/A Tags: constructive algorithms, greedy, *800 Ref: https://codeforces.com/contest/1461/submission/100920125

---
+++ codeforces/1461/A.cpp
@@ -0,0 +1,47 @@
+/*
+ * author : rits1272
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define print(x) cerr << "[" << #x << ": " << x << "]";
+#define println(x) cerr << "[" << #x << ": " << x << "]\n";
+#define show(v) for(int i = 0; i < (int)v.size(); i++) cout << v[i] << (i == (int)v.size()-1 ? "\n" : " ");
+#define deb(v) for(int i = 0; i < (int)v.size(); i++) {print(i) println(v[i])}
+#define pb push_back
+#define INF 1e18 + 5
+const int MOD = 1e9 + 7;
+
+void test_case(){
+	int n, k; cin >> n >> k;
+	string ans = "";
+	int cnt = 0;
+	for(int i = 0; i < n; i++){
+		if(cnt%3 == 0) ans += 'a';
+		if(cnt%3 == 1) ans += 'b';
+		if(cnt%3 == 2) ans += 'c';
+		cnt++;
+	}
+	cout << ans << "\n";
+}
+
+int32_t main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL); 
+	int tt; cin >> tt;
+	while(tt--){
+		test_case();
+	}
+	return 0; 
+}
+
+/*
+ * Only write code when you are sure of the approach.
+ * Do optimized thinking (Think which technique/observation can be used).
+ * Do post analysis after the contest.
+ * Upsolve the problems.
+ * Learn a technique and solve some problems if its new to you.
+ * DON'T GET STUCK ON ONE APPROACH
+ */ 
+
