ccc45b2800f8b512c2d55d0d05a66ffaa9ba8223
Ritik Jain <ritikjain1272@gmail.com>
2020-12-14 21:45:00 +0530

    Add solution for problem `A - String Transformation 1` Link: https://codeforces.com/contest/1383/problem/A Tags: dsu, graphs, greedy, sortings, strings, trees, two pointers, *1700 Ref: https://codeforces.com/contest/1383/submission/101188201

---
+++ codeforces/1383/A.cpp
@@ -0,0 +1,73 @@
+/*
+ * author : rits1272
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define print(x) cerr << "[" << #x << ": " << x << "]";
+#define println(x) cerr << "[" << #x << ": " << x << "]\n";
+#define show(v) for(int i = 0; i < (int)v.size(); i++) cout << v[i] << (i == (int)v.size()-1 ? "\n" : " ");
+#define deb(v) for(int i = 0; i < (int)v.size(); i++) {print(i) println(v[i])}
+#define pb push_back
+#define INF 1e18 + 5
+const int MOD = 1e9 + 7;
+
+void dfs(int u, vector<bool> &mark, vector<vector<int>> adj){
+	mark[u] = true;
+	for(int v: adj[u]){
+		if(!mark[v]){
+			dfs(v, mark, adj);
+		}
+	}
+}
+
+void test_case(){
+	map<char, set<char>> mp;
+	int n; cin >> n;
+	string a, b; cin >> a >> b;
+	vector<vector<int>> adj(20);
+	for(int i = 0; i < n; i++){
+		if(a[i] > b[i]){
+			cout << -1 << "\n";
+			return;
+		}
+		if(a[i] != b[i]){
+			adj[a[i]-'a'].pb(b[i]-'a');
+			adj[b[i]-'a'].pb(a[i]-'a');
+		}
+	}
+	int ans = 20;
+	
+	vector<bool> mark(20);
+	
+	for(int i = 0; i < 20; i++){
+		if(!mark[i]){
+			dfs(i, mark, adj);
+			--ans;
+		}
+	}
+	
+	cout << ans << '\n';
+	return;
+}
+
+int32_t main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL); 
+	int tt; cin >> tt;
+	while(tt--){
+		test_case();
+	}
+	return 0; 
+}
+
+/*
+ * Only write code when you are sure of the approach.
+ * Do optimized thinking (Think which technique/observation can be used).
+ * Do post analysis after the contest.
+ * Upsolve the problems.
+ * Learn a technique and solve some problems if its new to you.
+ * DON'T GET STUCK ON ONE APPROACH
+ */ 
+
